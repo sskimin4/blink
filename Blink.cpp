91be039059782ff181cf13ec2fda8f6081df6594
sskimin4 <37132420+sskimin4@users.noreply.github.com>
2018-03-09 02:19:02 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "Blink.h"
+
+Blink::Blink(int p){
+    pin=p;
+    pinMode(p,OUTPUT);
+}
+void Blink::on(int k){
+   digitalWrite(pin,LOW);
+   delay(k);
+ }
+void Blink::off(int k){
+    digitalWrite(pin,HIGH);
+    delay(k);
+  }
