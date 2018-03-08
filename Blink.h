358db19dc838d0e9de9189ed98d3c514b0109e1d
sskimin4 <37132420+sskimin4@users.noreply.github.com>
2018-03-09 01:59:38 +0900

    Add files via upload

---
+++ Blink.h
@@ -0,0 +1,14 @@
+#ifndef __Blink__H__
+#define __Blink__H__
+
+#include<Arduino.h>
+
+class Blink {
+  private:
+  short pin;
+  public:
+     Blink(int p);
+     void on(int k);
+     void off(int k);
+ };
+#endif
---
+++ blink.ino
@@ -0,0 +1,15 @@
+#include<Blink.h>
+
+Blink blink(D4);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\nBlink Version 1.0 Your_First_Name Your_Last_Name");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blink.on(500);
+  blink.off(300);
+}
